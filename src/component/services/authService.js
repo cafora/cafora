14d5312969340f9e20a1e01d1e9d9b7c63ddebe7
cafora <cafora32@gmail.com>
2025-04-12 19:00:09 +0530

    Update authService.js

--- src/component/services/authService.js
+++ src/component/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000';
+const API_URL = 'https://server-blpu.onrender.com';
 
 const authService = {
   login: async (credentials) => {
